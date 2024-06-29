f64a2246787d2937bf0e1bb8ae8c097165a9dbf4
devstyno <ddevstyno@gmail.com>
2024-06-29 17:43:07 +0000

    #

--- interface/vendable_par_kilogramme.py
+++
@@ -1,5 +0,0 @@
-class VendableParKilogramme:
-    """L'interface pour les produits qui se vendent par kilogramme."""
-    
-    def vendre(self, kg_vendus : int) -> float:
-        pass
--- interface/vendable_par_piece.py
+++
@@ -1,5 +0,0 @@
-class VendableParPiece:
-    """L'interface pour les produits qui se vendent par pièce."""
-
-    def vendre(self, pieces_vendues : int) -> float:
-        pass
