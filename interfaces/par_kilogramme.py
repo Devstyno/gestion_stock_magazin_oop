2800454034b856ffdbfcc577118b06a4436e1ac5
devstyno <ddevstyno@gmail.com>
2024-07-01 14:13:53 +0000

    suppression des interfaces

--- interfaces/par_kilogramme.py
+++
@@ -1,8 +0,0 @@
-class ParKilogramme:
-    """L'interface pour les produits qui se comptent par kilogramme."""
-    
-    def vendre(self, kg_vendus : float) -> float:
-        pass
-
-    def remplir_stock(self, qtte_kg : float) -> None:
-        pass
--- interfaces/par_piece.py
+++
@@ -1,8 +0,0 @@
-class ParPiece:
-    """L'interface pour les produits qui se comptent par pièce."""
-
-    def vendre(self, pieces_vendues : int) -> float:
-        pass
-
-    def remplir_stock(self, qtte_pieces : int) -> None:
-        pass
