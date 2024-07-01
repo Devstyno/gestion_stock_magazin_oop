866348c49f271bfbf6c137e8c7e4fe136d56d3e0
devstyno <ddevstyno@gmail.com>
2024-07-01 07:50:40 +0000

    definition des classes

--- modeles/electromenager.py
+++ modeles/electromenager.py
@@ -1,8 +1,6 @@
 from modeles.fournisseur import Fournisseur
 from modeles.article import Article
-from interfaces.par_piece import ParPiece
 
-# class Electromenager(Article, ParPiece): # heritage et implementation d'interface
 class Electromenager(Article): # heritage
     """La classe des articles élécroménagers."""
 
@@ -19,16 +17,7 @@ class Electromenager(Article): # heritage
     def pieces_en_stock(self, value : int):
         self.__pieces_en_stock = value
     
-    # methodes complementaires
-    # ## remplir le stock
-    # def remplir_stock(self, qtte : int):
-    #     self.pieces_en_stock += qtte
-    
-    # ## vendre
-    # def vendre(self, pieces_vendues: int) -> float:
-    #     super().vendre(pieces_vendues)
-    #     self.pieces_en_stock -= pieces_vendues
-    
+    # methodes complementaires    
     ## description de l'article
     def description(self): # redefinition de methode
         return f"{super().description()}\nStock : {self.pieces_en_stock} pieces"
--- modeles/primeur.py
+++ modeles/primeur.py
@@ -1,7 +1,5 @@
 from modeles.article import Article
-from interfaces.par_kilogramme import ParKilogramme
 
-# class Primeur(Article, ParKilogramme): # heritage et implementation d'interface
 class Primeur(Article): # heritage
     """La classe des primeurs."""
 
@@ -18,16 +16,7 @@ class Primeur(Article): # heritage
     def kg_en_stock(self, value : float):
         self.__kg_en_stock = value
     
-    # methodes complementaires
-    # ## remplir le stock
-    # def remplir_stock(self, qtte : float):
-    #     self.kg_en_stock += qtte
-    
-    # ## vendre
-    # def vendre(self, kg_vendus: float) -> float:
-    #     super().vendre(kg_vendus)
-    #     self.kg_en_stock -= kg_vendus
-    
+    # methodes complementaires    
     ## description de l'article
     def description(self): # redefinition de methode
         return f"{super().description()}\nStock : {self.kg_en_stock} kg"
